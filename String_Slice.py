91202f56966a4548af115c8b1dc81f6a520e2438
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-08-06 11:06:28 +0530

    String slicing

---
+++ String_Slice.py
@@ -0,0 +1,10 @@
+String = 'ASTRING'
+s1 = slice(3)
+s2 = slice(1,5,2)
+s3 = slice(-1,-12,-2)
+print("String slicing")
+print(String[s1])
+print(String[s2])
+print(String[s3])
+Lst = [50,70,30,20,90,10,50]
+print(Lst[-7::1])
