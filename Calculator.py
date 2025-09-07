1e47408929230f34519796212ba72d07a84a8528
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-07 14:15:57 +0530

    Calculator operations performed

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+a = float(input("Enter First Number: "))
+b = float(input("Enter Second Number: "))
+op = input("Enter any Operator (+,-,*,/): ")
+if op == '+':
+    print("Result is: ", a+b)
+elif op == '-':
+    print("Result is: ", a-b)
+elif op == '*':
+    print("Result is: ", a*b)
+elif op == '/':
+    if b != 0:
+        print("Result is: ", a/b)
+    else:
+        print("Division by zero not allowed")
+else:
+    print("Invalid Operator")
