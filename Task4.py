f4ba199770cf587ecbc9d475c558008b73f30c53
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-10 21:46:55 +0530

    Finds Factorial

--- Task4.py
+++ Task4.py
@@ -1,7 +1,17 @@
-import string
-s = "The quick brown fox jumps over the lazy dog"
-alphabet = set(string.ascii_lowercase)
-if set(s.lower()) >= alphabet:
-    print("Pangram")
-else:
-    print("Not Pangram")
+# import string
+# s = "The quick brown fox jumps over the lazy dog"
+# alphabet = set(string.ascii_lowercase)
+# if set(s.lower()) >= alphabet:
+#     print("Pangram")
+# else:
+#     print("Not Pangram")
+
+
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5)) 
+
