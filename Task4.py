628524d3a94e0eecb123b8f4694664db7d8ceeab
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-10 21:51:15 +0530

    Gives Common elements in lists

--- Task4.py
+++ Task4.py
@@ -44,7 +44,18 @@
 
 
 
-sentence = "I love Python programming"
-words = sentence.split()
-reversed_sentence = " ".join(words[::-1])
-print(reversed_sentence)
+# sentence = "I love Python programming"
+# words = sentence.split()
+# reversed_sentence = " ".join(words[::-1])
+# print(reversed_sentence)
+
+
+
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+common = []
+for x in list1:
+    if x in list2:
+        common.append(x)
+print("Common elements:", common)
