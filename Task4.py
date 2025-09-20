a7aae475daa74696c2ef04e49b9e7fd502519f50
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-20 14:10:28 +0530

    Max of 3 Numbers

--- Task4.py
+++ Task4.py
@@ -87,15 +87,15 @@
 
 
 
-n = 496
-div_sum = 0
-for i in range(1, n):
-    if n % i == 0:
-        div_sum += i
-if div_sum == n:
-    print(n, "is a Perfect Number")
-else:
-    print(n, "is not a Perfect Number")
+# n = 496
+# div_sum = 0
+# for i in range(1, n):
+#     if n % i == 0:
+#         div_sum += i
+# if div_sum == n:
+#     print(n, "is a Perfect Number")
+# else:
+#     print(n, "is not a Perfect Number")
 
 
 
@@ -151,3 +151,8 @@ else:
 
 
 
+a = int(input("Enter first: "))
+b = int(input("Enter second: "))
+c = int(input("Enter third: "))
+
+print("Maximum is:", max(a, b, c))
