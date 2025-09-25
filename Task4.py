2afd950ac6195d3f6acfc65d93dfae161025bba4
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-25 19:09:05 +0530

    Checks Perfect Number or Not

--- Task4.py
+++ Task4.py
@@ -87,25 +87,25 @@
 
 
 
-# n = 28
-# div_sum = 0
-# for i in range(1, n):
-#     if n % i == 0:
-#         div_sum += i
-# if div_sum == n:
-#     print(n, "is a Perfect Number")
-# else:
-#     print(n, "is not a Perfect Number")
+n = 496
+div_sum = 0
+for i in range(1, n):
+    if n % i == 0:
+        div_sum += i
+if div_sum == n:
+    print(n, "is a Perfect Number")
+else:
+    print(n, "is not a Perfect Number")
 
 
 
 
-a = 45
-b = 2
-result = 1
-for i in range(b):
-    result *= a
-print("Power = ", result)
+# a = 45
+# b = 2
+# result = 1
+# for i in range(b):
+#     result *= a
+# print("Power = ", result)
 
 
 
