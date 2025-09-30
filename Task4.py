eb1fa131cc5f15c03f14f908084606993b4660dc
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-09-30 21:42:41 +0530

    Divisible by 5 and 11

--- Task4.py
+++ Task4.py
@@ -240,16 +240,16 @@
 
 
 
-# n = int(input("Enter a Number: "))
-# if n % 5 == 0 and n % 11 == 0:
-#     print("Given Number is Divisible by 5 and 11")
-# else:
-#     print("Given Number is not Divisible by 5 and 11")
+n = int(input("Enter a Number: "))
+if n % 5 == 0 and n % 11 == 0:
+    print("It is Divisible by 5 and 11")
+else:
+    print("It is not Divisible by 5 and 11")
 
 
-n = int(input("Enter any Number: "))
-print("Square of Given Number = ", n**2)
-print("Cube of Given Number = ", n**3)
+# n = int(input("Enter any Number: "))
+# print("Square of Given Number = ", n**2)
+# print("Cube of Given Number = ", n**3)
 
 
 # ch = input("Enter any Character: ")
