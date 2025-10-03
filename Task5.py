64fe5165f9e4fcbe495dab3a20bc8fc931b8ab00
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-03 19:52:19 +0530

    Strong Numbers in range

--- Task5.py
+++ Task5.py
@@ -1,5 +1,12 @@
-for num in range(1,1001):
-    order = len(str(num))
-    s = sum(int(d)**order for d in str(num))
+# for num in range(1,1001):
+#     order = len(str(num))
+#     s = sum(int(d)**order for d in str(num))
+#     if s == num:
+#         print(num, end= " ")
+
+
+import math
+for num in range(1, 501):
+    s = sum(math.factorial(int(d)) for d in str(num))
     if s == num:
         print(num, end= " ")
