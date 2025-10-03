78e941cc5ae3a7e28d8da752c341d518a5c72646
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-03 20:09:43 +0530

    Diamond pattern

--- Task5.py
+++ Task5.py
@@ -47,14 +47,21 @@
 
 
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+# def gcd(a, b):
+#     while b:
+#         a, b = b, a % b
+#     return a
 
-def lcm(a, b):
-    return (a * b) // gcd(a, b)
+# def lcm(a, b):
+#     return (a * b) // gcd(a, b)
 
-a, b = 12, 18
-print("GCD:", gcd(a, b))
-print("LCM:", lcm(a, b))
+# a, b = 12, 18
+# print("GCD:", gcd(a, b))
+# print("LCM:", lcm(a, b))
+
+
+n = 5
+for i in range(1, n+1, 2):
+    print(" " * ((n-i)//2) + "*" * i)
+for i in range(n-2, 0, -2):
+    print(" " * ((n-i)//2) + "*" * i)
