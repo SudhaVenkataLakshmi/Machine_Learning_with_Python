aff6c818b943cd79f030a7cd247bb148d5a0d618
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-03 19:55:26 +0530

    Checks Magic Number or Not

--- Task5.py
+++ Task5.py
@@ -5,8 +5,16 @@
 #         print(num, end= " ")
 
 
-import math
-for num in range(1, 501):
-    s = sum(math.factorial(int(d)) for d in str(num))
-    if s == num:
-        print(num, end= " ")
+# import math
+# for num in range(1, 501):
+#     s = sum(math.factorial(int(d)) for d in str(num))
+#     if s == num:
+#         print(num, end= " ")
+
+
+def magic_number(n):
+    while n > 9:
+        n = sum(int(d) for d in str(n))
+        return n == 1
+num = int(input("Enter Any Number: "))
+print("Magic Number" if magic_number(num) else "Not Magic Number")
