02dfce2abf9e87fbb985a82fb9175b683c789d85
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-05 19:55:35 +0530

    Prime Factors

--- Task5.py
+++ Task5.py
@@ -68,7 +68,7 @@
 
 
 
-num = int(input("Enter a Number: "))
+num = int(input("Enter any Number: "))
 i = 2
 factors = []
 while num > 1:
@@ -77,4 +77,4 @@ while num > 1:
         num //= i
     else:
         i += 1
-print("Prime Factors of Given Number are :", factors)
+print("Prime Factors are:", factors)
