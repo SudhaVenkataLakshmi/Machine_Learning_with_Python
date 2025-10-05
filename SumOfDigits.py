dd79cf406b785e7f8ee1f5f7da38dfd6061ba812
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-05 19:52:28 +0530

    Sum of Digits

--- SumOfDigits.py
+++ SumOfDigits.py
@@ -1,7 +1,7 @@
-n = int(input("Enter any Number: "))
+n = int(input("Enter a Number: "))
 total = 0
 while n > 0:
     digit = n % 10
     total += digit
     n //= 10
-print("Sum of digits of Given Number: ", total)
+print("Sum of digits: ", total)
