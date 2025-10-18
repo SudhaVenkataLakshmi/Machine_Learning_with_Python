c6c67e840a6773451202b84c1c8130fe849cd752
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-10-18 20:37:16 +0530

    Count Vowels in Given String

--- CountVowels.py
+++ CountVowels.py
@@ -1,6 +1,6 @@
-s = input("ENter a String: ")
+s = input("Enter any String: ")
 count = 0
 for ch in s.lower():
     if ch in 'aeiou':
         count += 1
-print("Vowel Count: ",count)
+print("Count of Vowels in Given String: ",count)
