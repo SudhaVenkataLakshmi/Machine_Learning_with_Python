be7412e69da97f9541dd74e5951ae3b6497c3297
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-17 10:51:03 +0530

    Minimum Value in Array

--- MinArray.py
+++ MinArray.py
@@ -1,11 +1,11 @@
 a = [
-       [-1],
-      [2,3],
+    [-1],
+    [2,3],
     [1,-1,-3]
 ]
 ans = 0
 for i in range(len(a)-1,0,-1):
     for j in range(len(a[i])-1):
         temp = min(a[i][j],a[i][j+1])
-        a[i-1][j] = temp+a[i-1][j]
+        a[i-1][j] = temp + a[i-1][j]
 print(a[0][0])
