2e72996093934fead497d67335f8ce785de69614
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2025-12-19 20:16:42 +0530

    Checks Perfect Number or not

--- PerfectNumber.py
+++ PerfectNumber.py
@@ -4,9 +4,9 @@ for i in range(1, n):
     if n % i == 0:
         sum += i
 if sum == n:
-    print("It is Perfect Number")
+    print("Perfect Number")
 else:
-    print("It is Not Perfect Number")
+    print("Not Perfect Number")
 
 
 
