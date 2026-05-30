c25c928f322e1b61f70617be5e3a4db574ff0c94
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-05-30 15:52:18 +0530

    Practice on all topics

--- Practice.py
+++ Practice.py
@@ -1,4 +1,9 @@
-name = "Sushma"
-college = "ACET"
-print(name)
-print(college)
+# name = "Sushma"
+# college = "ACET"
+# print(name)
+# print(college)
+
+
+name = "Python"
+print(name[0])
+print(name[2])
