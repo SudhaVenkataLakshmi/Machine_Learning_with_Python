8ef01743e3f91ab7ead5febae7bc04fddf16e754
SudhaVenkataLakshmi <24P31F00F1@acet.ac.in>
2026-06-10 18:54:55 +0530

    Calculates LCM of given Numbers

---
+++ Sample2.py
@@ -0,0 +1,10 @@
+def lcm(a, b):
+    temp_a, temp_b = a, b
+
+    while temp_b:
+        temp_a, temp_b = temp_b, temp_a % temp_b
+
+    gcd = temp_a
+    return (a * b) // gcd
+
+print(lcm(12, 18))
